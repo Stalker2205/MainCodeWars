0c031eca5afec25fca5753c5cf1edd91dd20c58f
Nik <36083065+Stalker2205@users.noreply.github.com>
2022-08-26 14:55:25 +0300

    Добавьте файлы проекта.

---
+++ MainCodeWars/Program.cs
@@ -0,0 +1,17 @@
+﻿//Complete the function that takes two integers (a, b, where a < b) 
+//and return an array of all integers between the input parameters, including them.
+
+static int[] Between(int a, int b)
+{
+    int kol = 0;
+    for (int i = a;i<= b;i++)
+    {
+        kol++;
+    }
+        int[] rez = new int[kol];
+    for (int i = 0; a <= b; a++, i++)
+    {
+        rez[i] = a;
+    }
+    return rez;
+}
